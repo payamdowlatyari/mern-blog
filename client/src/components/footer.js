01bd1933d78943b16583980fe4166ea1cf421af3
Payam Dowlatyari <pdowlatyari@gmail.com>
2021-01-30 23:02:35 -0800

    initial commit small change UI

---
+++ client/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">
+          Copyright &copy; 2021 | Designed by <a href="https://www.payamd.com/" rel="noreferrer noopener" target="_blank">payamd.com</a>
+        </span>
+      </div>
+    </footer >
+  );
+}
---
+++ client/src/components/welcome.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => (
+  <div>
+
+    { /*Main jumbotron for a primary marketing message or call to action*/}
+    <div className="container">
+      <h1 className="display-4">Welcome to my blog!</h1>
+      <p><Link className="btn btn-outline-light btn-lg" to="/posts" role="button">Check out the blog posts &raquo;</Link></p>
+    </div>
+
+    { /*Example row of columns*/}
+    <div className="row text-justify">
+      <div className="col-md-4 card">
+
+      </div>
+      <div className="col-md-4 card">
+
+      </div>
+      <div className="col-md-4 card">
+
+      </div>
+    </div>
+  </div>
+);
